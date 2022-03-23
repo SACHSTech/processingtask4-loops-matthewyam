46bdbb110bd52119e7777d801457a8e0b2aba374
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2022-03-22 21:10:00 -0400

    gradient done+ 10x box almost

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,14 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+  int c1 = color(255);
+  int c2 = color(0);
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
   /** 
@@ -16,7 +17,9 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
+
+
   }
 
   /**
@@ -24,12 +27,25 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //10x10 box
+    stroke(0);
+    for (int boxX = width/10; boxX<=(width/2); boxX +=20) 
+      {line(boxX, 0, boxX, height/2);
+    }
+    stroke(0);
+    for (int boxY = height/10; boxY<=(height/2); boxY +=20) 
+      {line(width/2, boxY, 0, boxY);
+    }
+    //gradiant
+    stroke(0);
+    for (int gradiant = 20; gradiant <= (width/2); gradiant+=20) {
+      ;
+    
+      for (int i = 0; i<width/2; i++){
+      stroke(c1, i, c2);
+      line(i,0+height/2,i,height);
+      }
+    }
   }
   
   // define other methods down here.
